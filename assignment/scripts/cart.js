e13ab1c3e5ee5915433c3ea7cd36e8745030d6dc
Scott Pak <gpak86@gmail.com>
2023-11-17 18:41:31 -0500

    worked with kevin to complete activity inclass session

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,14 +1,52 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+const maxItems = 5;
 
+function addItem (item) {
+    if (isFull() === false) {
+        basket.push(item);
+        return true;
+    }
+    return false;
+}
+console.log(addItem('Apples'));
+console.log(addItem('Liver'));
+console.log(addItem('Jowel'));
+console.log(basket);
 
+function listItems () {
+    for (let itemFood of basket) {
+        console.log(itemFood);
+    }
+}
+listItems();
 
+function empty () {
+    basket = [];
+    //console.log(basket);
+}
+empty();
+console.log(basket);
 
+console.log(addItem('Apples'));
+console.log(addItem('Liver'));
+console.log(addItem('Jowel'));
 
+function isFull () {
+    if (basket.length < maxItems) {
+        return false;
+    }
+    return true;
+}
+console.log(isFull());
 
+console.log(addItem('Tiger Togue'));
+console.log(addItem('Monkey Foot'));
+console.log(addItem('Angel Tear'));
 
-
+console.log(basket);
 
 
 
